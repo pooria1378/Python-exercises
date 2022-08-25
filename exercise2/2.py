f73e8c8ab12b9b74188d0f36ec2955541ae07c6e
seyedpouria <seyedpoh@metropolia.fi>
2022-08-25 20:33:08 +0430

    exercise 2 part 2 is completed

---
+++ exercise2/2.py
@@ -0,0 +1,9 @@
+radius = input("please insert the radius : ")
+area = 3.14 * float(radius) * float(radius)
+print("area of the circle is : ", area)
+
+"""
+radius = float(input("radius of the circle : "))
+area = 3.14 * (radius ** 2)
+print(f"area : {area}")
+"""
