c93ba1d4d1fde6893806e1f430e23926c168f905
seyedpouria <seyedpoh@metropolia.fi>
2022-08-25 22:02:10 +0430

    exercise 2 part 3 is completed

---
+++ exercise2/3.py
@@ -0,0 +1,6 @@
+length = float(input("please insert length : "))
+width = float(input("please insert width : "))
+area = length * width
+perimeter = 2 * (length + width)
+print(f"area = {area} \nperimeter ={perimeter}")
+#print("area =", area, "\nperimeter=", perimeter)
