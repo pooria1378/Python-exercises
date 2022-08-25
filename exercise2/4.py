561c21ef58c1a20dcdf166784b33932f67d9801f
seyedpouria <seyedpoh@metropolia.fi>
2022-08-25 22:32:32 +0430

    exercise 2 part 4 is completed

---
+++ exercise2/4.py
@@ -0,0 +1,8 @@
+first_number = int(input("insert first number : "))
+second_number = int(input("insert second number : "))
+third_number = int(input("insert third number : "))
+sum_of_numbers = first_number + second_number + third_number
+product = first_number * second_number * third_number
+average = sum_of_numbers / 3
+#print("sum : ", sum_of_numbers,"product : ",product,"average : ",average)
+print(f"sum : {sum_of_numbers} \nproduct : {product} \naverage : {average}")
