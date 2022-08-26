f4b46b22f60e29c85c882694c962e349191b7b93
seyedpouria <seyedpoh@metropolia.fi>
2022-08-26 12:23:00 +0430

    exercise 2 part 6 is completed

---
+++ exercise2/6.py
@@ -0,0 +1,14 @@
+import random
+import sys
+
+r1 = random.randint(0,9)
+r2 = random.randint(0,9)
+r3 = random.randint(0,9)
+print(f"Random code {r1,r2,r3}")
+r1 = random.randint(1,6)
+r2 = random.randint(1,6)
+r3 = random.randint(1,6)
+r4 = random.randint(1,6)
+print(f"Random code {r1,r2,r3,r4}")
+
+
