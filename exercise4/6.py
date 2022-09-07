fb4cc23c9fb6dbe72bad9281935bd39a722dd4f5
pouria <s.poooria.h@gmail.com>
2022-09-07 20:31:17 +0300

    exercise 4 part 6 completed

---
+++ exercise4/6.py
@@ -0,0 +1,14 @@
+import random
+
+interval = 1000
+circle_points = 0
+square_points = 0
+for i in range(interval**2):
+    rand_x = random.uniform(-1,1)
+    rand_y = random.uniform(-1,1)
+    origin_dist = rand_x**2 + rand_y**2
+    if origin_dist<=1:
+        circle_points +=1
+    square_points +=1
+    pi = 4 * circle_points / square_points
+print("final estimation of pi: ", pi)
