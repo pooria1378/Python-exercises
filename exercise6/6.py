6df0c3730241f449fbb7c751651d2f1210fe5258
pouria <s.poooria.h@gmail.com>
2022-10-04 10:40:01 +0300

    exercise 6 part 6 completed

---
+++ exercise6/6.py
@@ -0,0 +1,5 @@
+#Price Per Square inch is calculated by dividing the price (P) of the pizza by its area
+def price(v1,v2):
+    #Aria=v1*v1*3.14
+    #price_per_squar=v2*Aria
+    return v1*v1*3.14*v2
